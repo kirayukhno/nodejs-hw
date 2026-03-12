59e50694685f978002898879e162295eccada8fd
Kira Yukhno <kira.yukhno123@gmail.com>
2026-03-12 23:45:53 +0200

    do the homework

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,11 @@
+import { HttpError } from 'http-errors';
+
+export const errorHandler = (err, req, res, next) => {
+  console.error('Error middleware:', err.message);
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message || err.name,
+    });
+  }
+  res.status(500).json({ message: err.message });
+};
