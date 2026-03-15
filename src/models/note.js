1965b0485f47a596e779bf1ef7144675fd73ee8b
Kira Yukhno <kira.yukhno123@gmail.com>
2026-03-15 21:32:03 +0200

    do the tasks

--- src/models/note.js
+++ src/models/note.js
@@ -1,5 +1,6 @@
 import { Schema } from "mongoose";
 import { model } from "mongoose";
+import { TAGS } from "../constants/tags.js";
 
 const noteSchema = new Schema(
   {
@@ -17,10 +18,7 @@ const noteSchema = new Schema(
     tag: {
       type: String,
       required: false,
-      enum: [
-        'Work', 'Personal', 'Meeting', 'Shopping',
-        'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo'
-      ],
+      enum: TAGS,
       default: 'Todo',
     },
   },
@@ -28,6 +26,10 @@ const noteSchema = new Schema(
     timestamps: true,
     versionKey: false,
   }
+);
+
+noteSchema.index(
+  { title: 'text', content: 'text' }
 );
 
 export const Note = model('Note', noteSchema);
