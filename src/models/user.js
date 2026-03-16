fcddff615bb385097418fb4d37dce417bf1c49e8
Kira Yukhno <kira.yukhno123@gmail.com>
2026-03-16 23:05:09 +0200

    do the homework

--- src/models/user.js
+++ src/models/user.js
@@ -4,7 +4,12 @@ const userSchema = new Schema(
   {
     username: { type: String, trim: true },
     email: { type: String, unique: true, required: true, trim: true },
-    password: { type: String, required: true}
+    password: { type: String, required: true },
+    avatar: {
+      type: String,
+      required: false,
+      default: "https://ac.goit.global/fullstack/react/default-avatar.jpg",
+    },
   },
   {
     timestamps: true,
