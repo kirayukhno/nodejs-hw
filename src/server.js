0d13e26ad371f368f87b252e5ffc412604f54583
Kira Yukhno <kira.yukhno123@gmail.com>
2026-03-16 13:02:24 +0200

    do the auth homework

--- src/server.js
+++ src/server.js
@@ -7,6 +7,10 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import { errors } from 'celebrate';
+import authRoutes from './routes/authRoutes.js';
+import cookieParser from 'cookie-parser';
+
+
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -14,7 +18,9 @@ const PORT = process.env.PORT ?? 3000;
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
+app.use(authRoutes);
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
